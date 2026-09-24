ae31ac99178212dea02bf869bfae6e6482c8c9c9
susila tropika <susilatropika8@gmail.com>
2026-09-24 10:46:45 +0700

    add automatic test

---
+++ src/utils/calculator.test.js
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest';
+import { add, subtract } from './calculator';
+describe('calculator', () => {
+    it('add', () => {
+        expect(add(1, 2)).toBe(3);
+    });
+
+    it('subtract', () => {
+        expect(subtract(7, 2)).toBe(5);
+    });
+});
